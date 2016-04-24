91fa00ed0210d9fa62d764944d4462f0cc333383
lars <larsen.fisch@gmail.com>
2016-04-24 14:36:17 -0400

    validation complete...I think

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,5 +1,6 @@
 (function ($) {
-    $("input").on("change paste keyup", function() {
+    //$("input").on("change paste keyup", function() {
+    $("input").on("change paste", function() {
         updateCartPrice();
     });
 
@@ -13,9 +14,14 @@
             var ingredientList = $(this).find("ul");
 
             ingredientList.children("li").each(function () {
+                var form = $(this).find(".form-inline");
                 var count = $(this).find("input").val();
                 var price = $(this).find(".price").text();
-                recipeTotal += count * price;
+                
+                if (validateInput(form, count)) {
+                    removeError(form);
+                    recipeTotal += count * price;
+                }
             });
             cartTotal += recipeTotal;
             recipeTotal = parseFloat(recipeTotal).toFixed(2);
@@ -24,5 +30,29 @@
 
         cartTotal = parseFloat(cartTotal).toFixed(2);
         $(".cart-price").text("$" + cartTotal);
+    }
+    
+    function validateInput(form, value) {
+        if (value == null | value == undefined || isNaN(value) || value === "") {
+            addError(form, "Invalid: not a number");
+            return false;
+        } else if (value < 0) {
+            addError(form, "Invalid: number below zero");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    function addError(element, error) {
+        removeError(element); // remove previous error
+        element.addClass("has-error");
+        var error = $("<span class=\"help-block\"></span>").text(error);
+        element.append(error);
+    }
+    
+    function removeError(element) {
+        element.find($(".help-block")).remove();
+        element.removeClass("has-error");
     }
 })(jQuery);
